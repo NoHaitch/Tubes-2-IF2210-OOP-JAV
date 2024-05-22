532ff435cceff87e7dc192182b2069538b6ea8a3
Francisco <franciscotrianto@gmail.com>
2024-05-22 13:16:52 +0700

    feat: reworked DataLoader interface, finished PluginLoader

--- src/main/java/javva/tubes2/dataLoader/DataLoader.java
+++ src/main/java/javva/tubes2/dataLoader/DataLoader.java
@@ -1,5 +1,7 @@
 package javva.tubes2.dataLoader;
 
+import javva.tubes2.Player;
+
 /**
  *  Data Loader Interface  <br>
  *  Save and Load data from file <br>
@@ -7,21 +9,33 @@ package javva.tubes2.dataLoader;
  */
 public interface DataLoader {
     /**
-     * Save data to filepath
-     *
-     * @param data object that will be saved
-     * @param filePath relative path to result file
-     * @throws Exception data not correct, etc
+     * Save player data to file
+     * @param player Player data
+     * @param filePath relative path to result folder
+     * @throws Exception file not found, failed to save
      */
-    void saveData(Object data, String filePath) throws Exception;
+    void savePlayer(Player player, String filePath) throws Exception;
 
     /**
-     * Load data from filepath
-     *
-     * @param filePath relative path to result file
-     * @param className fully qualified name of the class
-     * @return data object
-     * @throws Exception data not correct, corrupted save
+     * Save game data to file
+     * @param filePath relative path to result folder
+     * @throws Exception file not found, failed to save
      */
-    Object loadData(String filePath, String className) throws Exception;
+    void saveGameState(String filePath) throws Exception;
+
+    /**
+     * Load player data from file
+     * @param filePath relative path to result folder
+     * @return Player object
+     * @throws Exception file not found, failed to load
+     */
+    Player loadPlayer(String filePath) throws Exception;
+
+    /**
+     * Load game state from file
+     * @param filePath relative path to result folder
+     * @return Game state object
+     * @throws Exception file not found, failed to load
+     */
+    Object loadGameState(String filePath) throws Exception;
 }
