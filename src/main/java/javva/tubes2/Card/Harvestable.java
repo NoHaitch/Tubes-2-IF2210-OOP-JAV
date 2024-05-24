9327e18983c869fbd84d92cfa66f1167246565b0
zaki_yudhistira <zakiyudhistira.itb69@gmail.com>
2024-05-24 13:45:49 +0700

    feat : bear attack

--- src/main/java/javva/tubes2/Card/Harvestable.java
+++ src/main/java/javva/tubes2/Card/Harvestable.java
@@ -2,7 +2,22 @@ package javva.tubes2.Card;
 
 public class Harvestable extends Card{
     private Product product;
+    private Boolean protected_flag;
+    private Boolean attacked;
     
+    public Harvestable(String nameString, String tyString, String imgPath, Product prod){
+        super(nameString, tyString, imgPath);
+        product = prod;
+        protected_flag = false;
+        attacked = false;
+    }
+
+    public Harvestable(String nameString, String tyString){
+        super(nameString, tyString, "");
+        product = new Product(nameString, tyString, tyString, null, null);
+        protected_flag = false;
+    }
+
     public Product getProduct() {
         return product;
     }
@@ -18,14 +33,20 @@ public class Harvestable extends Card{
         return false;
     }
 
-    public Harvestable(String nameString, String tyString, String imgPath, Product prod){
-        super(nameString, tyString, imgPath);
-        product = prod;
+    public Boolean getProtectedFlag() {
+        return protected_flag;
     }
 
-    public Harvestable(String nameString, String tyString){
-        super(nameString, tyString, "");
-        product = new Product(nameString, tyString, tyString, null, null);
+    public void setProtectedFlag(Boolean protected_flag) {
+        this.protected_flag = protected_flag;
+    }
+
+    public Boolean getAttacked() {
+        return attacked;
+    }
+
+    public void setAttacked(Boolean attacked) {
+        this.attacked = attacked;
     }
 }
 
